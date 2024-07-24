e7ff8efe92282f68d148153e2075404d47952fba
nmatos <nmatos@pursuit.org>
2024-07-24 16:51:39 -0400

    did bonus for many to many

--- learn_hotels.sql
+++ learn_hotels.sql
@@ -12,4 +12,4 @@ UPDATE rooms SET vacant=FALSE WHERE room_num=202 AND hotel_id=(SELECT id FROM ho
 
 UPDATE rooms SET hotel_id=(SELECT id from hotels WHERE name='Grand Budapest Hotel') WHERE hotel_id=7;
 
-
+SELECT guests.name AS guest_name, hotels.name AS hotel_name, rooms.name AS room_name, rooms_guests.stay_dates, rooms.price, hotels.state FROM guests JOIN rooms_guests ON guests.id = rooms_guests.guest_id JOIN rooms ON rooms.id = rooms_guests.room_id JOIN hotels ON hotels.id = rooms.hotel_id ORDER BY guest_name;
